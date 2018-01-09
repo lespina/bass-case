27b070b4be4e0e0013c92862f74c846b75f790d7
Luke Espina <luke.espina@gmail.com>
2018-01-09 17:18:42 -0500

    Fix bug where queue item buttons overlayed each other unnecessarily

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109204507) do
+ActiveRecord::Schema.define(version: 20180109220720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followee_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followee_id"], name: "index_follows_on_followee_id"
+    t.index ["follower_id", "followee_id"], name: "index_follows_on_follower_id_and_followee_id", unique: true
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
